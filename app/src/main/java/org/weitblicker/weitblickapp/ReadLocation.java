2241d60749645e839c9e0228d026f5fc859a6784
mohism <ghost9999100@gmail.com>
2016-10-20 21:45:21 +0200

    display the project information on Layout called  (activity_project_contents)

--- app/src/main/java/org/weitblicker/weitblickapp/ReadLocation.java
+++ app/src/main/java/org/weitblicker/weitblickapp/ReadLocation.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 
 /**
  * Created by mohism on 19.10.2016.
- */
+ *  this Class display the project information on Layout called  (activity_project_contents)  */
 
 public class ReadLocation extends AppCompatActivity {
 
@@ -20,10 +20,21 @@ public class ReadLocation extends AppCompatActivity {
     private TextView l_latitude;
     private TextView l_adition;
     private  TextView l_comments;
+    private TextView p_name ;
+    private TextView p_description ;
+    private  TextView p_title ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.location);
+        setContentView(R.layout.activity_project_contents);
+
+
+
+        // call the Project data
+
+        p_name = (TextView) findViewById(R.id.name);
+        p_description = (TextView) findViewById(R.id.diescription);
+        p_title = (TextView) findViewById(R.id.title);
 
 
 
@@ -39,6 +50,36 @@ public class ReadLocation extends AppCompatActivity {
         l_latitude = (TextView) findViewById(R.id.latitude);
         l_adition = (TextView) findViewById(R.id.adition);
         l_comments = (TextView) findViewById(R.id.comments);
+
+
+
+        // display the project's details
+
+        Project p1=new Project(0,"ahmed","address","AMO","DSD");
+
+        p_name.setText("Nmae:"+ " "+p1.getName());
+        p_description.setText("Description:"+ " "+p1.getDescription());
+        p_title.setText("Abstract:"+ " "+p1.getAbstract());
+
+
+       //display the location
+
+        Location l=new Location(0,"ahmed",0,"address",0,"AMO","","","","DSD");
+
+
+
+        l_town.setText("Town:"+ " "+l.getTown());
+        l_street.setText("Street:" + " "+l.getStreet()+"No: "+ " "+ l.getstreetNo());
+        l_country.setText("Country:" + l.getCountry());
+        l_postal_code.setText("" + l.getPostalCode());
+        l_longitude.setText("" + l.getLongitude());
+        l_latitude.setText("" + l.getLatitude());
+        l_adition.setText("" +l.getAdition());
+        l_comments.setText("" + l.getComments());
+
+
     }
+
+
 
 }
